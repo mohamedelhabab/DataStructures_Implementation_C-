57b9714784149da1b26ff47138090053ea0b716c
Mohamed Elhabbab From Delta <dev.mohamed.elhabab@gmail.com>
2018-09-24 09:39:19 +0200

    Add Generic ArrayList

---
+++ Collections/GenericArrayList.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Collections
+{
+   public class GenericArrayList<T>
+    {
+        //array type must be object because arrayList is not a fixed type Collection
+        T[] list;
+
+        /// <summary>
+        /// Total count for items in arraylist 
+        /// </summary>
+        public int Length { get; private set; }
+
+        public GenericArrayList()
+        {
+            list = new T[0];
+        }
+
+        /// <summary>
+        /// Add new item to ArrayList
+        /// </summary>
+        /// <param name="item">new item</param>
+        /// <returns>The new item index in the ArrayList</returns>
+        public int Add(T item)
+        {
+            T[] newList = new T[list.Length + 1];
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            newList[newList.Length - 1] = item;
+
+            list = newList;
+            Length = list.Length;
+
+            return list.Length - 1;
+
+        }
+
+        /// <summary>
+        /// Insert item at specific index
+        /// </summary>
+        /// <param name="index">The index to insert the new item</param>
+        /// <param name="item">item</param>
+        /// <returns></returns>
+        public int Insert(int index, T item)
+        {
+            T[] newList = new T[list.Length + 1];
+            newList[index] = item;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (newList[i] == null)
+                {
+                    newList[i] = list[i];
+                }
+                else
+                {
+                    newList[i + 1] = list[i];
+                }
+            }
+
+            list = newList;
+            Length = list.Length;
+
+            return list.Length - 1;
+        }
+
+        /// <summary>
+        /// Remove item at specific index
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            int newListIndex = 0;
+
+            T[] newList = new T[list.Length - 1];
+
+            for (int listPtr = 0; listPtr < list.Length; listPtr++)
+            {
+                if (listPtr != index)
+                {
+                    newList[newListIndex] = list[listPtr];
+
+                    newListIndex++;
+                }
+            }
+
+            list = newList;
+            Length = list.Length;
+        }
+
+        /// <summary>
+        /// Get item at specific index
+        /// </summary>
+        /// <param name="index">index for item</param>
+        /// <returns></returns>
+        public T GetItem(int index)
+        {
+            return list[index];
+        }
+
+        /// <summary>
+        /// Print all items to console
+        /// </summary>
+        public void PrintItems()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+        //exhausted search used here
+        //Future Work : use a better search algorithm !
+
+        /// <summary>
+        /// Get the index of specific item
+        /// </summary>
+        /// <param name="item">item</param>
+        /// <returns></returns>
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].ToString() == item.ToString())
+                    return i;
+            }
+
+            return -1;
+
+        }
+        /// <summary>
+        /// Replace item with a new one
+        /// </summary>
+        /// <param name="index">item index to be replaced</param>
+        /// <param name="item">the new item value</param>
+
+        public void ReplaceWith(int index, T item)
+        {
+            list[index] = item;
+        }
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,5 @@
 ﻿using Collections;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
@@ -12,43 +8,42 @@ namespace ConsoleApp2
 
       public  static void Main(string[] args)
         {
-            ArrayList arrList=new ArrayList();
+            //ArrayList arrList=new ArrayList();
 
-            //adding some items
-            arrList.Add(20);
-            arrList.Add(30);
-            arrList.Add(40);
-            arrList.Add(50);
+            ////adding some items
+            //arrList.Add(20);
+            //arrList.Add(30);
+            //arrList.Add(40);
+            //arrList.Add(50);
+            //arrList.Add("End");
+            ////insert item in specific index
+            //arrList.Insert(2, 5000);
+            //Console.WriteLine("Original Array\n");
+            ////print all items
+            //arrList.PrintItems();
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("New Array\n");
+            ////remove item at specific index
+            //arrList.RemoveAt(5);
+            //arrList.ReplaceWith(0,7000);
+            ////print all items
+            //arrList.PrintItems();
+            //Console.WriteLine("-----------------------------------------------");
 
-            arrList.Add("End");
+            //Console.WriteLine($"index of {30} is {arrList.IndexOf(30)}");
+            //Console.WriteLine($"index of {50} is {arrList.IndexOf(50)}");
+            //Console.WriteLine($"index of End is {arrList.IndexOf("End")}");
+            //Console.Read();
 
+            GenericArrayList<int> genericArrayList = new GenericArrayList<int>();
 
-            //insert item in specific index
-            arrList.Insert(2, 5000);
+            genericArrayList.Add(5);
+            genericArrayList.Add(1);
 
-            Console.WriteLine("Original Array\n");
-
-            //print all items
-            arrList.PrintItems();
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("New Array\n");
-
-            //remove item at specific index
-            arrList.RemoveAt(5);
-            arrList.ReplaceWith(0,7000);
-
-            //print all items
-            arrList.PrintItems();
-
-            Console.WriteLine("-----------------------------------------------");
-
-            Console.WriteLine($"index of {30} is {arrList.IndexOf(30)}");
-            Console.WriteLine($"index of {50} is {arrList.IndexOf(50)}");
-            Console.WriteLine($"index of End is {arrList.IndexOf("End")}");
-
-
-
+            int sum = genericArrayList.GetItem(0) + genericArrayList.GetItem(1);
+            Console.WriteLine("Sum is {0}",sum);
             Console.Read();
+
 
         }
     }
